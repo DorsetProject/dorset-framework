ddb6d7257a029a94d09a407801cbc248d735059a
Caitlyn Bishop <caitlyn.bishop@jhuapl.edu>
2017-08-31 20:28:10 -0400

    Updating copyright header from 2016 to 2017

--- components/session-service/src/main/java/edu/jhuapl/dorset/simplesessionservice/SimpleSessionService.java
+++ components/session-service/src/main/java/edu/jhuapl/dorset/simplesessionservice/SimpleSessionService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 The Johns Hopkins University Applied Physics Laboratory LLC
+ * Copyright 2017 The Johns Hopkins University Applied Physics Laboratory LLC
  * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
